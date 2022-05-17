ca9ed9745e960be6d9b237512ae393b03b0a41c2
PierreJacobs <pierre.jacobs@student.unamur.be>
2022-05-17 09:09:04 +0200

    a bit of cleaning files

--- src/main.py
+++ src/main.py
@@ -3,45 +3,20 @@ from os import listdir
 from os.path import join, abspath, dirname
 
 import face_recognition
-from PIL import Image, ImageDraw, ImageFilter
+from PIL import Image
+
+import utils
 
 ROOT = dirname(abspath(r"./static"))
 IMAGE_PATH = join(ROOT, r"static/img")
 IN = r'in'
 OUT = r'out'
 
-def apply_blur_mask(pil_image: Image, face_locations, /, radius=50) -> Image:
-    """Applies a blur mask at given locations
-
-    :param pil_image: PIL image
-    :type pil_image: Image
-
-    :param face_locations: locations of faces on `pil_image`
-    :type face_locations: nympy[ndarray]
-
-    :param radius: blur radius, defaults to 50
-    :type radius: int, optional
-
-    :return: blurred image
-    :rtype: Image
-    """
-    for (top, right, bottom, left) in face_locations:
-        cropped_image = pil_image.crop((left, top, right, bottom))
-        c_i_size = cropped_image.size
-
-        mask = Image.new("L", c_i_size, 0)
-        draw = ImageDraw.Draw(mask)
-        draw.pieslice((0, 0, c_i_size[0], c_i_size[1]), 0, 360, fill=255)
-
-        blurred_image = cropped_image.filter(ImageFilter.GaussianBlur(radius=radius))
-        cropped_image.paste(blurred_image, mask=mask)
-        pil_image.paste(cropped_image, (left, top, right, bottom))
-
-    return pil_image
-
 def main() -> None:
     """Runs an example
     """
+
+    args = utils.cli_parser()
 
     for image_name in listdir(join(IMAGE_PATH, IN)):
 
@@ -49,12 +24,13 @@ def main() -> None:
         face_locations = face_recognition.face_locations(image)
 
         pil_image = Image.fromarray(image)
-        pil_image = apply_blur_mask(pil_image, face_locations)
 
-        try:
-            pil_image.save(join(IMAGE_PATH, OUT, image_name))
-        except FileNotFoundError as error:
-            print(error)
+        if args.blur:
+            pil_image = utils.apply_blur_mask(pil_image, face_locations)
+            try:
+                pil_image.save(join(IMAGE_PATH, OUT, image_name))
+            except FileNotFoundError as error:
+                print(error)
 
 if __name__ == "__main__":
     sys.exit(main())
---
+++ src/utils.py
@@ -0,0 +1,42 @@
+import argparse
+
+from PIL import Image, ImageDraw, ImageFilter
+
+def apply_blur_mask(pil_image: Image, face_locations, /, radius=50) -> Image:
+    """Applies a blur mask at given locations
+
+    :param pil_image: PIL image
+    :type pil_image: Image
+
+    :param face_locations: locations of faces on `pil_image`
+    :type face_locations: nympy[ndarray]
+
+    :param radius: blur radius, defaults to 50
+    :type radius: int, optional
+
+    :return: blurred image
+    :rtype: Image
+    """
+    for (top, right, bottom, left) in face_locations:
+        cropped_image = pil_image.crop((left, top, right, bottom))
+        c_i_size = cropped_image.size
+
+        mask = Image.new("L", c_i_size, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.pieslice((0, 0, c_i_size[0], c_i_size[1]), 0, 360, fill=255)
+
+        blurred_image = cropped_image.filter(ImageFilter.GaussianBlur(radius=radius))
+        cropped_image.paste(blurred_image, mask=mask)
+        pil_image.paste(cropped_image, (left, top, right, bottom))
+
+    return pil_image
+
+def cli_parser() -> argparse.Namespace:
+    """Returns a parsed version of parameters given to the script
+
+    :return: parsed arguments
+    :rtype: argparse.Namespace
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--blur', help="blur the output images", action="store_true")
+    return parser.parse_args()
